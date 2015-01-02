d8fb67ef9cd7a90dfa4a7dbbaf35340a2ef21fbd
davidyin1212 <davidyin1212@gmail.com>
2015-01-01 21:54:16 -0500

    first commit

---
+++ SecurityParser.py
@@ -0,0 +1,12 @@
+from HTMLParser import HTMLParser
+# create a subclass and override the handler methods
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print "Encountered a start tag:", tag
+        if (tag == "h1") {
+        	
+        }
+    def handle_endtag(self, tag):
+        print "Encountered an end tag :", tag
+    def handle_data(self, data):
+        print "Encountered some data  :", data
